e93b1dd029651540c1e75425b48f544686eaf964
Galo Garlejo <galo@penbrothers.com>
2019-06-11 01:08:30 +0800

    Front end functions and server side bootstrapping

---
+++ public/javascripts/custom.js
@@ -0,0 +1,17 @@
+// hide 2nd page
+$("#categories").hide();
+
+// SPA using jquery
+$("#home-nav").click(function(){
+    $("#cat-nav-parent").removeClass('active') 
+    $("#home-nav-parent").addClass('active') 
+    $("#categories").hide();
+    $("#home").show();
+});
+
+$("#cat-nav").click(function(){
+    $("#home-nav-parent").removeClass('active') 
+    $("#cat-nav-parent").addClass('active') 
+    $("#home").hide();
+    $("#categories").show();
+});
